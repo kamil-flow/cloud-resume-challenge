569d755f3a258dd18d95c33d0581a167c0bf3b2f
kamil-flow <wol.kam.sky@gmail.com>
2026-01-11 16:25:14 +0000

    added a googl It photo

--- frontend/src/data/projectImages.js
+++ frontend/src/data/projectImages.js
@@ -1,8 +1,9 @@
 import awsDraw from 'images/aws-draw.png'
+import google_it from 'images/google-it.webp'
 
 
 export const projectImages = {
     'kindle-highlights': awsDraw,
-    
+    'start-with-it': google_it,
     
 }
